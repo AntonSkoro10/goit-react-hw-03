8d73bfc71aca9406db093a23bfc8c464a2d8a1c2
AntonSkoro10 <antonskoro112@gmail.com>
2024-07-19 18:15:19 +0300

    gg

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,39 +1,38 @@
-import { useId } from "react"
-import { nanoid } from "nanoid"
-import { Formik, Form, Field, ErrorMessage } from "formik"
-import * as Yup from "yup"
-import css from "./ContactForm.module.css"
-
+import { useId } from "react";
+import { nanoid } from "nanoid";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import css from "./ContactForm.module.css";
 
 export default function ContactForm({ addContact }) {
-    const nameId = useId()
-    const numberId = useId()
+    const nameId = useId();
+    const numberId = useId();
     const PhoneSchema = Yup.object().shape({
         name: Yup.string()
-            .min(3, "To short")
-            .max(50, "To long")
+            .min(3, "Too short")
+            .max(50, "Too long")
             .required("Required"),
         number: Yup.string()
-            .min(3, "To short")
-            .max(50, "To long")
+            .min(3, "Too short")
+            .max(50, "Too long")
             .required("Required"),
-    })
+    });
 
     const handleSubmit = (values, actions) => {
         addContact({
             id: nanoid(),
             name: values.name,
-            number: values.number
-        })
-        actions.resetForm()
-    }
+            number: values.number,
+        });
+        actions.resetForm();
+    };
+
     return (
         <Formik
             initialValues={{ name: "", number: "" }}
             validationSchema={PhoneSchema}
             onSubmit={handleSubmit}
         >
-
             <Form className={css.form}>
                 <div className={css.container}>
                     <label htmlFor={nameId}>Name</label>
@@ -42,11 +41,11 @@ export default function ContactForm({ addContact }) {
                 </div>
                 <div className={css.container}>
                     <label htmlFor={numberId}>Number</label>
-                    <Field type="tel" name="name" id={numberId} />
+                    <Field type="tel" name="number" id={numberId} />
                     <ErrorMessage name="number" component="span" />
                 </div>
                 <button className={css.formBtn} type="submit">Add Contact</button>
             </Form>
-            </Formik>
-    )
+        </Formik>
+    );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,17 @@
-import Contacts from "../Contacts/Contacts"
+import Contact from "../Contacts/Contacts"
+import css from "./ContactList.module.css"
 
 export default function ContactList({ contacts, deleteContact }) {
     return (
-        <ul className={ }>
+        <ul className={css.list}>
             {contacts.map(({ id, name, number }) => (     
-                <li>
-                    
+                <li className={css.item} key={id}>
+                    <Contact
+                        id={id}
+                        name={name}
+                        number={number}
+                        deleteContact={deleteContact}
+                    />                    
             </li>
             ))}
         </ul>
